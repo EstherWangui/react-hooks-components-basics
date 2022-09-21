bf366a3ef8fe15724f0c32c5d8f2f5301f89a7ac
EstherWangui <esther.nganga@student.moringaschool.com>
2022-09-21 11:57:49 +0300

    completed assignment

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,19 @@
 import React from "react";
+//const Comment = () => <div>Naturally, I agree with this article.</div>
+//const reader = () => <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+
+
 
 function Article() {
   return (
+    
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
-}
 
-export default Article;
+    //<div>Naturally, I agree with this article.</div>
+      )
+}
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+
+}
+export default Article
